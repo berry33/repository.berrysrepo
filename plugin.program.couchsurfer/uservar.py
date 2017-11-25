224e247bb8dd785b1e4128e582e79d578660bb5e
berry33 <32066976+berry33@users.noreply.github.com>
2017-11-25 00:23:10 -0500

    revert wizard update

--- plugin.program.couchsurfer/uservar.py
+++ plugin.program.couchsurfer/uservar.py
@@ -105,10 +105,10 @@ ENABLE         = 'Yes'
 # Url to notification file
 NOTIFICATION   = 'http://berryburchett.net/couch/couchsurfer/note.txt'
 # Use either 'Text' or 'Image'
-HEADERTYPE     = 'Text'
-HEADERMESSAGE  = 'What's New:'
+HEADERTYPE     = 'text'
+HEADERMESSAGE  = 'Whats New'
 # url to image if using Image 424x180
 HEADERIMAGE    = ''
 # Background for Notification Window
-BACKGROUND     = 'http://berryburchett.net/couch/couchsurfer/solid-black-background-hd-16.jpeg'
+BACKGROUND     = ''
 #########################################################
